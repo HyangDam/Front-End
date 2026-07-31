f4ecd8744f8faeacecb8edf9f41ce4d835c1c3ca
ParkSeohyeon <tjgus4394@gmail.com>
2026-07-31 20:41:10 +0900

    feat: 홈 향수 목업 데이터 추가

---
+++ src/app/(tabs)/home/_consts/homePerfumes.const.ts
@@ -0,0 +1,68 @@
+import type { PerfumeT } from "@/types/perfume";
+
+export const HOME_PERFUMES: PerfumeT[] = [
+  {
+    id: 1,
+    name: "상탈 33",
+    brand: "LE LABO",
+    brandKr: "르 라보",
+    price: "₩420,000",
+    img: "https://fimgs.net/mdimg/perfume/375x500.30551.jpg",
+  },
+  {
+    id: 2,
+    name: "미스 디올 블루밍 부케",
+    brand: "DIOR",
+    brandKr: "디올",
+    price: "₩195,000",
+    img: "https://fimgs.net/mdimg/perfume/375x500.12207.jpg",
+  },
+  {
+    id: 3,
+    name: "플라워밤",
+    brand: "VIKTOR & ROLF",
+    brandKr: "빅터앤롤프",
+    price: "₩235,000",
+    img: "https://fimgs.net/mdimg/perfume/375x500.3907.jpg",
+  },
+  {
+    id: 4,
+    name: "블랙 오피움",
+    brand: "YVES SAINT LAURENT",
+    brandKr: "입생로랑",
+    price: "₩215,000",
+    img: "https://fimgs.net/mdimg/perfume/375x500.46907.jpg",
+  },
+  {
+    id: 5,
+    name: "샹스 오 떵드르",
+    brand: "CHANEL",
+    brandKr: "샤넬",
+    price: "₩260,000",
+    img: "https://fimgs.net/mdimg/perfume/375x500.18188.jpg",
+  },
+  {
+    id: 6,
+    name: "리브르",
+    brand: "YVES SAINT LAURENT",
+    brandKr: "입생로랑",
+    price: "₩245,000",
+    img: "https://fimgs.net/mdimg/perfume/375x500.68107.jpg",
+  },
+  {
+    id: 7,
+    name: "라이프 이즈 뷰티풀",
+    brand: "LANCÔME",
+    brandKr: "랑콤",
+    price: "₩185,000",
+    img: "https://fimgs.net/mdimg/perfume/375x500.68428.jpg",
+  },
+  {
+    id: 8,
+    name: "오 로즈",
+    brand: "DIPTYQUE",
+    brandKr: "딥티크",
+    price: "₩175,000",
+    img: "https://fimgs.net/mdimg/perfume/375x500.22388.jpg",
+  },
+];
